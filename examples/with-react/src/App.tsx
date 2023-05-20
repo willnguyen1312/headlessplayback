68571aa2b14b937b80c54d666a914a3f078df63e
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-05-20 09:02:47 -0300

    chore: add frameworks examples

---
+++ examples/with-react/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <p className="text-xl color-violet">Hello there</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ examples/with-vue/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <p class="text-xl color-cyan">Hello there</p>
+  </div>
+</template>
