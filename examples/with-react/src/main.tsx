5f4668dbe4e61ae9cc76a6149631b4d22cef96eb
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-05-21 15:06:14 -0300

    feat: bring back the StrictMode support 🚀

--- examples/with-react/src/main.tsx
+++ examples/with-react/src/main.tsx
@@ -1,12 +1,15 @@
 import "virtual:uno.css"
 import "@unocss/reset/tailwind.css"
 
+import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import App from "./Routes.tsx"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
