254b06184056af762fc1b92de4b11e25e4f39041
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-05-23 18:07:17 -0300

    docs: add basic documentation site

---
+++ packages/docs/src/.vitepress/config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vitepress"
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Headless Playback",
+  description: "A simple yet complete playback library designed for UI frameworks or even without",
+  base: "/headless-playback/",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Examples", link: "/markdown-examples" },
+    ],
+
+    sidebar: [
+      {
+        text: "Examples",
+        items: [
+          { text: "Markdown Examples", link: "/markdown-examples" },
+          { text: "Runtime API Examples", link: "/api-examples" },
+        ],
+      },
+    ],
+
+    socialLinks: [{ icon: "github", link: "https://github.com/vuejs/vitepress" }],
+  },
+})
