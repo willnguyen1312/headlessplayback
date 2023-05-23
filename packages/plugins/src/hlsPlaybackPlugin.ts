66d17a8a198ef637e196caf041b02e199da4a84c
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-05-23 20:08:59 -0300

    chore: update hls plugin

--- packages/plugins/src/hlsPlaybackPlugin.ts
+++ packages/plugins/src/hlsPlaybackPlugin.ts
@@ -32,10 +32,34 @@ export const hlsPlaybackPlugin: Plugin<Partial<HlsConfig>> = {
 
       hls.loadSource(src)
       hls.attachMedia(playbackElement)
+
       hls.on(Hls.Events.MANIFEST_PARSED, (_, data) => {
         store.setState({
           resolutions: data.levels.map((level) => level.height.toString()),
         })
+      })
+
+      // Reference - https://github.com/video-dev/hls.js/blob/master/docs/API.md#fifth-step-error-handling
+      hls.on(Hls.Events.ERROR, function (_, data) {
+        if (data.fatal) {
+          switch (data.type) {
+            case Hls.ErrorTypes.MEDIA_ERROR:
+              console.info("fatal media error encountered, try to recover")
+              hls?.recoverMediaError()
+              break
+            case Hls.ErrorTypes.NETWORK_ERROR:
+              console.error("fatal network error encountered", data)
+              // All retries and media options have been exhausted.
+              // Immediately trying to restart loading could cause loop loading.
+              // Consider modifying loading policies to best fit your asset and network
+              // conditions (manifestLoadPolicy, playlistLoadPolicy, fragLoadPolicy).
+              break
+            default:
+              // cannot recover
+              hls?.destroy()
+              break
+          }
+        }
       })
     }
   },
