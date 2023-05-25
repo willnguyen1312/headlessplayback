6d070611bc3a5d48f1a07ab06fb079b9497114c1
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-05-25 18:34:01 -0300

    chore: finalize core API

--- packages/core/src/playback.ts
+++ packages/core/src/playback.ts
@@ -4,6 +4,19 @@ import { clamp } from "./utils"
 export interface InternalPlaybackState {
   currentTime: number
   duration: number
+  seeking: boolean
+  waiting: boolean
+  stalled: boolean
+  ended: boolean
+  playbackRate: number
+  paused: boolean
+  muted: boolean
+  volume: number
+  isPictureInPicture: boolean
+  buffered?: TimeRanges
+  textTracks?: TextTrackList
+  selectedTrackIndex: number
+  isPictureInPictureSupported: boolean
 }
 
 export type CustomPlaybackAction<T> = (arg: T & { id: string }) => void
@@ -16,6 +29,13 @@ export interface CustomPlaybackActions {}
 
 export type PlaybackActions = {
   setCurrentTime: (value: number) => void
+  setPlaybackRate: (value: number) => void
+  setVolume: (value: number) => void
+  setMuted: (value: boolean) => void
+  setPaused: (value: boolean) => void
+  disableTrack: () => void
+  enableTrack: (index: number) => void
+  setPictureInPicture: (value: boolean) => Promise<void | PictureInPictureWindow>
 } & CustomPlaybackActions
 
 export type PlaybackState = InternalPlaybackState & CustomPlaybackState
@@ -57,11 +77,23 @@ type PlaybackFunc = {
 
 const cleanupCallbackMap = new Map<string, Set<CleanupFunc>>()
 const playbackActivatedSet = new WeakSet<HTMLMediaElement>()
+const isPictureInPictureSupported = document && "pictureInPictureEnabled" in document
 
 export const playback: PlaybackFunc = ({ id }) => {
   const store = createStore<PlaybackState>({
     currentTime: 0,
     duration: 0,
+    seeking: false,
+    volume: 1,
+    waiting: false,
+    ended: false,
+    paused: true,
+    playbackRate: 1,
+    stalled: false,
+    muted: false,
+    isPictureInPicture: false,
+    selectedTrackIndex: -1,
+    isPictureInPictureSupported,
   })
 
   const abortController = new AbortController()
@@ -69,29 +101,68 @@ export const playback: PlaybackFunc = ({ id }) => {
 
   let playbackElement: HTMLMediaElement | undefined
 
-  function handleTimeUpdate() {
-    store.setState({
-      currentTime: playbackElement?.currentTime,
-    })
-  }
-
-  function handleLoadedMetadata() {
-    store.setState({
-      duration: playbackElement?.duration,
-    })
-  }
-
-  function handleSeeking() {
-    store.setState({
-      currentTime: playbackElement?.currentTime,
-    })
-  }
-
   const playbackActions: PlaybackActions = {
     setCurrentTime: (value: number) => {
       if (playbackElement) {
         playbackElement.currentTime = clamp(value, 0, playbackElement.duration)
       }
+    },
+    setPlaybackRate: (value: number) => {
+      if (playbackElement) {
+        playbackElement.playbackRate = value
+      }
+    },
+    setVolume: (value: number) => {
+      if (playbackElement) {
+        playbackElement.volume = clamp(value, 0, 1)
+      }
+    },
+    setMuted: (value: boolean) => {
+      if (playbackElement) {
+        playbackElement.muted = value
+      }
+    },
+    setPaused: (value: boolean) => {
+      if (playbackElement) {
+        if (value) {
+          playbackElement.pause()
+        } else {
+          playbackElement.play().catch(() => {
+            // ignore
+          })
+        }
+      }
+    },
+    disableTrack: () => {
+      const { selectedTrackIndex } = store.getState()
+      if (playbackElement && playbackElement.textTracks) {
+        playbackElement.textTracks[selectedTrackIndex].mode = "disabled"
+      }
+      store.setState({ selectedTrackIndex: -1 })
+    },
+    enableTrack: (index: number) => {
+      playbackActions.disableTrack()
+      if (playbackElement && playbackElement.textTracks) {
+        playbackElement.textTracks[index].mode = "showing"
+      }
+      store.setState({ selectedTrackIndex: index })
+    },
+    setPictureInPicture: (value: boolean) => {
+      if (!isPictureInPictureSupported || !playbackElement || !(playbackElement instanceof HTMLVideoElement)) {
+        return Promise.reject()
+      }
+
+      const isPictureInPicture = store.getState().isPictureInPicture
+
+      if (isPictureInPicture === value) {
+        return Promise.resolve()
+      }
+
+      if (value) {
+        return playbackElement.requestPictureInPicture()
+      }
+
+      return document.exitPictureInPicture()
     },
   }
 
@@ -115,9 +186,165 @@ export const playback: PlaybackFunc = ({ id }) => {
     }
 
     playbackElement = _playbackElement
-    playbackElement?.addEventListener("loadedmetadata", handleLoadedMetadata, { signal })
-    playbackElement?.addEventListener("seeking", handleSeeking, { signal })
-    playbackElement?.addEventListener("timeupdate", handleTimeUpdate, { signal })
+    playbackElement?.addEventListener(
+      "timeupdate",
+      () => {
+        store.setState({
+          currentTime: playbackElement?.currentTime,
+        })
+      },
+      { signal },
+    )
+    playbackElement?.addEventListener(
+      "durationchange",
+      () => {
+        store.setState({
+          duration: playbackElement?.duration,
+        })
+      },
+      { signal },
+    )
+    playbackElement?.addEventListener("progress", () => {
+      store.setState({
+        buffered: playbackElement?.buffered,
+      })
+    })
+    playbackElement?.addEventListener(
+      "seeking",
+      () => {
+        store.setState({
+          currentTime: playbackElement?.currentTime,
+        })
+      },
+      { signal },
+    )
+    playbackElement?.addEventListener(
+      "seeking",
+      () => {
+        store.setState({
+          seeking: true,
+        })
+      },
+      { signal },
+    )
+    playbackElement?.addEventListener(
+      "seeked",
+      () => {
+        store.setState({
+          seeking: false,
+        })
+      },
+      { signal },
+    )
+
+    for (const event of ["waiting", "loadstart"]) {
+      playbackElement?.addEventListener(
+        event,
+        () => {
+          store.setState({
+            waiting: true,
+            paused: false,
+          })
+        },
+        { signal },
+      )
+    }
+
+    playbackElement?.addEventListener(
+      "loadeddata",
+      () => {
+        store.setState({
+          waiting: false,
+        })
+      },
+      { signal },
+    )
+    playbackElement?.addEventListener(
+      "ratechange",
+      () => {
+        store.setState({
+          playbackRate: playbackElement?.playbackRate ?? 1,
+        })
+      },
+      { signal },
+    )
+    playbackElement?.addEventListener(
+      "stalled",
+      () => {
+        store.setState({
+          stalled: true,
+        })
+      },
+      { signal },
+    )
+    playbackElement?.addEventListener(
+      "ended",
+      () => {
+        store.setState({
+          ended: true,
+        })
+      },
+      { signal },
+    )
+    playbackElement?.addEventListener(
+      "paused",
+      () => {
+        store.setState({
+          paused: true,
+        })
+      },
+      { signal },
+    )
+    playbackElement?.addEventListener(
+      "play",
+      () => {
+        store.setState({
+          paused: false,
+        })
+      },
+      { signal },
+    )
+    playbackElement?.addEventListener(
+      "play",
+      () => {
+        store.setState({
+          volume: playbackElement?.volume ?? 1,
+          muted: playbackElement?.muted ?? false,
+        })
+      },
+      { signal },
+    )
+    playbackElement?.addEventListener(
+      "enterpictureinpicture",
+      () => {
+        store.setState({
+          isPictureInPicture: true,
+        })
+      },
+      { signal },
+    )
+    playbackElement?.addEventListener(
+      "leavepictureinpicture",
+      () => {
+        store.setState({
+          isPictureInPicture: false,
+        })
+      },
+      { signal },
+    )
+
+    // Tracks events
+    for (const event of ["addtrack", "removetrack", "change"]) {
+      playbackElement?.textTracks?.addEventListener(
+        event,
+        () => {
+          store.setState({
+            textTracks: playbackElement?.textTracks,
+          })
+        },
+        { signal },
+      )
+    }
 
     store.setState({
       currentTime: playbackElement?.currentTime,
--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -4,3 +4,13 @@ export function clamp(value: number, min: number, max: number) {
 
 // Credit: https://github.com/kentor/flush-promises
 export const flushPromises = () => new Promise((resolve) => setTimeout(resolve))
+
+export const convertTimeRangeToArray = (value: TimeRanges) => {
+  const result: [number, number][] = []
+
+  for (let index = 0; index < value.length; ++index) {
+    result.push([value.start(index), value.end(index)])
+  }
+
+  return result
+}
