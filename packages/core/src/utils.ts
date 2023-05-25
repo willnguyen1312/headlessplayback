aa941cfd7b4c8c3b86adcb4cf98b9663d5d1f8fe
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-05-25 13:04:23 -0300

    🤸‍♂️

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -1,3 +1,6 @@
 export function clamp(value: number, min: number, max: number) {
   return Math.min(Math.max(value, min), max)
 }
+
+// Credit: https://github.com/kentor/flush-promises
+export const flushPromises = () => new Promise((resolve) => setTimeout(resolve))
