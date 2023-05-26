cb59f76d3da124618a008a9ae77aeea9348d2917
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-05-26 10:14:59 -0300

    🎢

--- examples/with-react/src/main.tsx
+++ examples/with-react/src/main.tsx
@@ -3,13 +3,11 @@ import "@unocss/reset/tailwind.css"
 
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
-import App from "./Routes.tsx"
+
+import App from "./App.tsx"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
