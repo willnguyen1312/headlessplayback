1b9ac55c92e639c73c239dafe550dc997a04f9ec
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-06-09 11:06:15 -0300

    🚀

--- packages/docs/src/components/BundleSize.vue
+++ packages/docs/src/components/BundleSize.vue
@@ -1,8 +1,8 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import size from "../../../../size.json"
 defineProps<{ func: string; pkg: string }>()
-</script>
+</script> -->
 
 <template>
-  <span>{{ size[pkg][func] }}</span>
+  <!-- <span>{{ size[pkg][func] }}</span> -->
 </template>
