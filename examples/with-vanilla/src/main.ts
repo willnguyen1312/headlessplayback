4e2ddfa1ce99838e91d6c7415432a21fd28d8e2c
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-06-09 18:12:17 -0300

    Lazy load it 🚀

--- examples/with-vanilla/src/main.ts
+++ examples/with-vanilla/src/main.ts
@@ -1,9 +1,7 @@
-import "virtual:uno.css"
 import "@unocss/reset/tailwind.css"
+import "virtual:uno.css"
 
 import { playback } from "@headlessplayback/core"
-import { hlsPlaybackPlugin } from "@headlessplayback/hls-plugin"
-playback.use(hlsPlaybackPlugin)
 
 const source1 = "https://storage.googleapis.com/shaka-demo-assets/angel-one-hls/hls.m3u8"
 const source2 = "https://cdn.jwplayer.com/manifests/pZxWPRg4.m3u8"
@@ -13,25 +11,33 @@ const currentTime = document.getElementById("currentTime") as HTMLParagraphEleme
 const resolutions = document.getElementById("resolutions") as HTMLParagraphElement
 const duration = document.getElementById("duration") as HTMLParagraphElement
 const switchBtn = document.getElementById("switch") as HTMLButtonElement
+const activate = document.getElementById("activate") as HTMLButtonElement
 
-switchBtn.addEventListener("click", () => {
-  currentSource = currentSource === source1 ? source2 : source1
-  result.playbackActions.load({
-    source: currentSource,
+const hitIt = async () => {
+  const { hlsPlaybackPlugin } = await import("@headlessplayback/hls-plugin")
+  playback.use(hlsPlaybackPlugin)
+
+  const result = playback({
+    id: "video",
   })
-})
 
-const result = playback({
-  id: "video",
-})
+  switchBtn.addEventListener("click", () => {
+    currentSource = currentSource === source1 ? source2 : source1
+    result.playbackActions.load({
+      source: currentSource,
+    })
+  })
 
-result.subscribe(({ state }) => {
-  currentTime.innerText = `Current time: ${state.currentTime.toString()}`
-  duration.innerText = `Duration: ${state.duration.toString()}`
-  resolutions.innerText = `Levels: ${state.levels.map((level) => level.height).join(", ")}`
-})
+  result.subscribe(({ state }) => {
+    currentTime.innerText = `Current time: ${state.currentTime.toString()}`
+    duration.innerText = `Duration: ${state.duration.toString()}`
+    resolutions.innerText = `Levels: ${state.levels.map((level) => level.height).join(", ")}`
+  })
 
-result.activate()
-result.playbackActions.load({
-  source: source1,
-})
+  result.activate()
+  result.playbackActions.load({
+    source: source1,
+  })
+}
+
+activate.addEventListener("click", hitIt)
