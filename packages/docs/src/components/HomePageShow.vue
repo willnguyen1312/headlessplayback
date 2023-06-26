6e3bb10bbcee0fa10c7d8bb4cb976f477747454b
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-06-26 13:37:31 -0300

    Update Homepage show component

--- packages/docs/src/components/HomePageShow.vue
+++ packages/docs/src/components/HomePageShow.vue
@@ -32,24 +32,22 @@ const updateTab = (tabName: string) => {
 
 <template>
   <div class="min-h-xl m-x-auto rounded-4 container mt-4 flex h-[700px] flex-col bg-white p-4 font-sans text-black">
-    <div class="border-gray-200">
-      <nav class="-mb-px flex space-x-8" aria-label="Tabs">
-        <a
-          v-for="tab in tabs"
-          :key="tab.name"
-          :href="tab.href"
-          :aria-current="tab.current ? 'page' : undefined"
-          class="whitespace-nowrap border-b-2 px-1 py-4 text-sm font-medium"
-          :class="{
-            'border-indigo-500 text-indigo-600': tab.current,
-            'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700': !tab.current,
-          }"
-          @click="updateTab(tab.name)"
-        >
-          {{ tab.name }}
-        </a>
-      </nav>
-    </div>
+    <nav class="-mb-px flex space-x-8 border-gray-200" aria-label="Tabs">
+      <a
+        v-for="tab in tabs"
+        :key="tab.name"
+        :href="tab.href"
+        :aria-current="tab.current ? 'page' : undefined"
+        class="whitespace-nowrap border-b-2 px-1 py-4 text-sm font-medium"
+        :class="{
+          'border-indigo-500 text-indigo-600': tab.current,
+          'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700': !tab.current,
+        }"
+        @click="updateTab(tab.name)"
+      >
+        {{ tab.name }}
+      </a>
+    </nav>
 
     <div class="mt-4">
       <component :is="components[tabs.find((tab) => tab.current)!.name as PlaybackName]" />
