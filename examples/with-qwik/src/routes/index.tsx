39f55851bd5a1cff2cd5f7fc8a0e671710d054ff
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-06-26 13:36:33 -0300

    chore: add qwik example link to doc

--- examples/with-qwik/src/routes/index.tsx
+++ examples/with-qwik/src/routes/index.tsx
@@ -32,50 +32,30 @@ const App = component$(() => {
 
   return (
     <div class="p-4">
-      <div class="sm:hidden">
-        <label for="tabs" class="sr-only">
-          Select a tab
-        </label>
-        <select
-          id="tabs"
-          name="tabs"
-          class="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-          value={currentValue.value}
-        >
-          {tabs.value.map((tab) => (
-            <option key={tab.name}>{tab.name}</option>
-          ))}
-        </select>
-      </div>
-
-      <div class="hidden sm:block">
-        <div class="border-gray-200">
-          <nav class="-mb-px flex space-x-8" aria-label="Tabs">
-            {tabs.value.map((tab) => (
-              <a
-                key={tab.name}
-                href={tab.href}
-                preventdefault:click
-                onClick$={() => {
-                  tabs.value = tabs.value.map((prevTab) => ({
-                    ...prevTab,
-                    current: prevTab.name === tab.name,
-                  }))
-                }}
-                class={cls(
-                  tab.current
-                    ? "border-indigo-500 text-indigo-600"
-                    : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
-                  "whitespace-nowrap border-b-2 px-1 py-4 text-sm font-medium",
-                )}
-                aria-current={tab.current ? "page" : undefined}
-              >
-                {tab.name}
-              </a>
-            ))}
-          </nav>
-        </div>
-      </div>
+      <nav class="-mb-px flex space-x-8 border-gray-200" aria-label="Tabs">
+        {tabs.value.map((tab) => (
+          <a
+            key={tab.name}
+            href={tab.href}
+            preventdefault:click
+            onClick$={() => {
+              tabs.value = tabs.value.map((prevTab) => ({
+                ...prevTab,
+                current: prevTab.name === tab.name,
+              }))
+            }}
+            class={cls(
+              tab.current
+                ? "border-indigo-500 text-indigo-600"
+                : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
+              "whitespace-nowrap border-b-2 px-1 py-4 text-sm font-medium",
+            )}
+            aria-current={tab.current ? "page" : undefined}
+          >
+            {tab.name}
+          </a>
+        ))}
+      </nav>
 
       <div class="mt-4">
         <ComponentToRender />
