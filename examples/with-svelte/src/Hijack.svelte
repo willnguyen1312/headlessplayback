d2bff4cbe705688386a583317f65ec1b88e07bab
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-06-26 10:16:14 -0300

    Update Svelte example 💞 (#57)

---
+++ examples/with-svelte/src/Hijack.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import type { PlaybackState } from "@headlessplayback/core"
+  import { hijackPlaybackPlugin } from "@headlessplayback/hijack-plugin"
+  import { usePlayback } from "@headlessplayback/svelte"
+  import { onDestroy, onMount } from "svelte"
+
+  usePlayback.use(hijackPlaybackPlugin)
+  const id = "hijack"
+
+  const {
+    activate,
+    playbackActions,
+    playbackState: _playbackState,
+  } = usePlayback({
+    id,
+  })
+
+  let playbackState: PlaybackState
+
+  const unsubscribe = _playbackState.subscribe((value: PlaybackState) => {
+    playbackState = value
+  })
+
+  onMount(() => {
+    activate()
+    playbackActions.hijack({ direction: playbackState.direction, duration: 1000, frequency: 4 })
+  })
+
+  onDestroy(() => {
+    unsubscribe()
+  })
+
+  const jumpTo = (time: number) => {
+    playbackActions.setCurrentTime(time)
+  }
+</script>
+
+<video class="display-none" hidden {id} controls> <track kind="captions" /></video>
+
+<p>Current time: {playbackState.currentTime}</p>
+<p>Duration: {playbackState.duration}</p>
+<p>Direction: {playbackState.direction}</p>
+
+<div class="flex flex-col items-start">
+  <button on:click={() => jumpTo(playbackState.currentTime + 5)}>Next 5s</button>
+  <button
+    on:click={() => {
+      playbackActions.setPaused(!playbackState.paused)
+    }}>{playbackState.paused ? "Play" : "Pause"}</button
+  >
+  <button on:click={() => jumpTo(playbackState.currentTime - 5)}>Prev 5s</button>
+  <button
+    on:click={() => {
+      playbackActions.setDirection({ direction: playbackState.direction === "forward" ? "backward" : "forward" })
+    }}>Toggle direction</button
+  >
+</div>
---
+++ examples/with-svelte/src/Hls.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+  import type { PlaybackState } from "@headlessplayback/core"
+  import { hlsPlaybackPlugin } from "@headlessplayback/hls-plugin"
+  import { usePlayback } from "@headlessplayback/svelte"
+  import { onDestroy, onMount } from "svelte"
+  usePlayback.use(hlsPlaybackPlugin)
+
+  let id = "hls"
+
+  const {
+    activate,
+    playbackActions,
+    playbackState: _playbackState,
+  } = usePlayback({
+    id,
+  })
+
+  const source1 = "https://storage.googleapis.com/shaka-demo-assets/angel-one-hls/hls.m3u8"
+  const source2 = "https://cdn.jwplayer.com/manifests/pZxWPRg4.m3u8"
+
+  let source = source1
+
+  let playbackState: PlaybackState
+
+  const unsubscribe = _playbackState.subscribe((value: PlaybackState) => {
+    playbackState = value
+  })
+
+  onMount(() => {
+    activate()
+  })
+
+  onDestroy(() => {
+    unsubscribe()
+  })
+
+  const handleSource = async (source: string) => {
+    playbackActions.loadHlsSource({
+      source,
+    })
+  }
+
+  $: handleSource(source)
+</script>
+
+<div class="border-emerald border-1 h-[400px] w-[600px]">
+  <video {id} class="h-full w-full" controls>
+    <track kind="captions" />
+  </video>
+</div>
+
+<p>Current time: {playbackState.currentTime}</p>
+<p>Duration: {playbackState.duration}</p>
+
+<strong>Levels: {playbackState.levels.map((level) => level.height).join(", ")}</strong>
+
+<div class="flex flex-col items-start">
+  <button
+    on:click={() => {
+      if (source === source1) {
+        source = source2
+      } else {
+        source = source1
+      }
+    }}>Switch stream</button
+  >
+
+  <button
+    on:click={() => {
+      playbackActions.setCurrentTime(playbackState.currentTime + 5)
+    }}>Next 5s</button
+  >
+  <button
+    on:click={() => {
+      playbackActions.setCurrentTime(playbackState.currentTime - 5)
+    }}>Prev 5s</button
+  >
+</div>
