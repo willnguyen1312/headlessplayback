d279347bc1110084806d684b61b1521671ac5585
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-06-27 15:54:33 -0300

    Update colors for react example

--- examples/with-preact/src/Dash.tsx
+++ examples/with-preact/src/Dash.tsx
@@ -69,7 +69,7 @@ function Dash() {
 
   return (
     <>
-      <div class="border-emerald border-1 h-[400px] w-[600px]">
+      <div class="border-fuchsia border-1 h-[400px] w-[600px]">
         <video class="h-full w-full" id={id} controls></video>
       </div>
 
