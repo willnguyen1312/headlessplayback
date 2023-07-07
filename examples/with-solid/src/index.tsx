0ff31b29e261fe43b0f793167748f76b937845c1
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-07-07 09:47:37 -0300

    Fix typing in Solid examples 💞 (#80)

--- examples/with-solid/src/index.tsx
+++ examples/with-solid/src/index.tsx
@@ -1,14 +1,7 @@
-import "virtual:uno.css"
 import "@unocss/reset/tailwind.css"
 import { render } from "solid-js/web"
+import "virtual:uno.css"
 import App from "./App"
 
-const root = document.getElementById("root")
-
-if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-  throw new Error(
-    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?",
-  )
-}
-
+const root = document.getElementById("root") as HTMLElement
 render(() => <App />, root)
