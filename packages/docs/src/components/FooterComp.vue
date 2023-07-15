cc61e69951664e4183eb584335e68b58fb487d21
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-07-15 09:14:12 -0300

    Update footer link 💞 (#83)

--- packages/docs/src/components/FooterComp.vue
+++ packages/docs/src/components/FooterComp.vue
@@ -1,11 +1,11 @@
 <template>
   <p class="mt-4 text-center text-2xl">
-    Made with ❤️‍🔥 by
+    Made with 💞 by
     <a
       class="dark:text-violet font-bold text-[#7c447c]"
       target="_blank"
       rel="noopener noreferrer"
-      href="https://namnguyen.design"
+      href="https://namnguyen.design/about"
       >Nam Nguyen</a
     >
   </p>
