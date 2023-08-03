66edf49b1d594fa443588f5e1c2d025323d0380d
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-08-03 13:20:58 -0300

    Add normal example to Svelte example 💞

--- examples/with-svelte/src/Normal.svelte
+++ examples/with-svelte/src/Normal.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
   import type { PlaybackState } from "@headlessplayback/core"
-  import { hlsPlaybackPlugin } from "@headlessplayback/hls-plugin"
   import { usePlayback } from "@headlessplayback/svelte"
   import { onDestroy, onMount } from "svelte"
-  usePlayback.use(hlsPlaybackPlugin)
 
-  let id = "hls"
+  let id = "normal"
 
   const {
     activate,
@@ -15,17 +13,15 @@
     id,
   })
 
-  const source1 =
-    "https://storage.googleapis.com/shaka-demo-assets/angel-one-hls/hls.m3u8"
-  const source2 = "https://cdn.jwplayer.com/manifests/pZxWPRg4.m3u8"
-
-  let source = source1
-
   let playbackState: PlaybackState
 
   const unsubscribe = _playbackState.subscribe((value: PlaybackState) => {
     playbackState = value
   })
+
+  const jumpTo = (time: number) => {
+    playbackActions.setCurrentTime(time)
+  }
 
   onMount(() => {
     activate()
@@ -34,22 +30,15 @@
   onDestroy(() => {
     unsubscribe()
   })
-
-  const jumpTo = (time: number) => {
-    playbackActions.setCurrentTime(time)
-  }
-
-  const handleSource = async (source: string) => {
-    playbackActions.loadHlsSource({
-      source,
-    })
-  }
-
-  $: handleSource(source)
 </script>
 
 <div class="border-fuchsia border-1 h-[400px] w-[600px]">
-  <video {id} class="h-full w-full" controls>
+  <video
+    {id}
+    class="h-full w-full"
+    src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+    controls
+  >
     <track kind="captions" />
   </video>
 </div>
@@ -57,23 +46,7 @@
 <p>Current time: {playbackState.currentTime}</p>
 <p>Duration: {playbackState.duration}</p>
 
-<strong
-  >Levels: {playbackState.levels
-    .map((level) => level.height)
-    .join(", ")}</strong
->
-
 <div class="flex flex-col items-start space-y-1">
-  <button
-    on:click={() => {
-      if (source === source1) {
-        source = source2
-      } else {
-        source = source1
-      }
-    }}>Switch stream</button
-  >
-
   <button
     class="rounded-md bg-violet-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600"
     on:click={() => jumpTo(playbackState.currentTime + 5)}>Next 5s</button
