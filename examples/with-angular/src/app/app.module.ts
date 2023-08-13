673f88b00ece7ac7648b9ac79f6f2e67d305229d
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-08-13 10:19:58 -0300

    Add dash player to angular example 💞

--- examples/with-angular/src/app/app.module.ts
+++ examples/with-angular/src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from "@angular/core"
 import { BrowserModule } from "@angular/platform-browser"
 
 import { AppComponent } from "./app.component"
+import { PlaybackDashComponent } from "./dash.component"
 import { PlaybackHlsComponent } from "./hls.component"
 import { PlaybackNormalComponent } from "./normal.component"
 
 @NgModule({
-  declarations: [AppComponent, PlaybackNormalComponent, PlaybackHlsComponent],
+  declarations: [
+    AppComponent,
+    PlaybackNormalComponent,
+    PlaybackHlsComponent,
+    PlaybackDashComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
