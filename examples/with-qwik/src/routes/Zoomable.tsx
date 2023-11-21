826ab653da9959d6cfc1484792f7fcbe7a2dabca
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-11-21 08:17:42 -0400

    Update Qwik example 💞

--- examples/with-qwik/src/routes/Zoomable.tsx
+++ examples/with-qwik/src/routes/Zoomable.tsx
@@ -1,5 +1,6 @@
-import { $, component$ } from "@builder.io/qwik"
+import { $, component$, useSignal, useVisibleTask$ } from "@builder.io/qwik"
 import { usePlayback } from "@headlessplayback/qwik"
+import { zoomablePlaybackPlugin } from "@headlessplayback/zoomable-plugin"
 
 const id = "normal"
 
@@ -19,9 +20,23 @@ const CurrentTime = component$(() => {
   return <p>Current time: {playbackState.currentTime}</p>
 })
 
-const Hijack = component$(() => {
+const Zoomable = component$(() => {
   const { playbackActions, playbackState, use } = usePlayback({
     id,
+  })
+
+  const videoContainerRef = useSignal<HTMLDivElement>()
+
+  useVisibleTask$(async () => {
+    await use(zoomablePlaybackPlugin)
+
+    playbackActions.createZoomablePlayback?.({
+      container: videoContainerRef.value as HTMLDivElement,
+    })
+
+    playbackActions.setEnableZoom({
+      enableZoom: true,
+    })
   })
 
   const jumpNext5s = $(() => {
@@ -32,14 +47,20 @@ const Hijack = component$(() => {
     playbackActions.setCurrentTime(playbackState.currentTime - 5)
   })
 
+  const togglePlayback = $(() => {
+    playbackActions.setPaused(!playbackState.paused)
+  })
+
   return (
     <>
-      <div class="border-fuchsia border-1 h-[400px] w-[600px]">
+      <div
+        ref={videoContainerRef}
+        class="border-fuchsia border-1 h-[337.5px] w-[600px]"
+      >
         <video
           src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-          class="h-full w-full"
+          class="h-full w-full cursor-zoom-in"
           id={id}
-          controls
         ></video>
       </div>
 
@@ -49,19 +70,27 @@ const Hijack = component$(() => {
       <div class="flex flex-col items-start space-y-1 ">
         <button
           class="rounded-md bg-violet-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600"
-          onClick$={jumpNext5s}
-        >
-          Next 5s
-        </button>
-        <button
-          class="rounded-md bg-violet-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600"
           onClick$={jumpPrev5s}
         >
           Prev 5s
+        </button>
+
+        <button
+          class="rounded-md bg-violet-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600"
+          onClick$={togglePlayback}
+        >
+          {playbackState.paused ? "Play" : "Pause"}
+        </button>
+
+        <button
+          class="rounded-md bg-violet-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600"
+          onClick$={jumpNext5s}
+        >
+          Next 5s
         </button>
       </div>
     </>
   )
 })
 
-export default Hijack
+export default Zoomable
